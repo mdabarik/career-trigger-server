005d4d3c25af8f5bb9f6b5c379654f9535b56dfe
mdabarik <mdabarik19@gmail.com>
2026-01-30 00:56:13 +0600

    project reorganizing

--- src/app/modules/posts/posts.route.ts
+++ src/app/modules/posts/posts.route.ts
@@ -3,7 +3,7 @@ import { postController } from './posts.controller';
 
 const router = Router();
 
-router.get('/posts', postController.GetAllPosts);
+router.get('/', postController.GetAllPosts);
 router.get('/posts/:id', postController.GetPostById);
 
 export default router;
--- src/app/utils/sendResponse.ts
+++ src/app/utils/sendResponse.ts
@@ -1,14 +1,7 @@
 import { Response } from 'express';
-
-type TResponse<T> = {
-    success: boolean;
-    message?: string;
-    statusCode: number;
-    data: T;
-};
+import { TResponse } from '../types/response.type';
 
 const sendResponse = <T>(res: Response, data: TResponse<T>) => {
-    console.log('send response:', data);
     res.status(data?.statusCode).json({
         statusCode: data?.statusCode,
         success: data.success,
